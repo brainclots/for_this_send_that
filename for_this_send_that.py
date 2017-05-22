81570589a5385671906afbdac8fffd25d87c9bf4
brainclots <brainclots@hotmail.com>
2017-05-22 08:08:22 -0700

    Added show & rearchitected excel parsing

--- for_this_send_that.py
+++ for_this_send_that.py
@@ -15,7 +15,7 @@ Author:
             |__] |  \ | |  | | \|    | \_ |___ |__|  |   /__
             Brian.Klotz@nike.com
 
-Version:    0.5
+Version:    0.8
 Date:       May 2017
 '''
 import argparse
@@ -30,18 +30,29 @@ parser = argparse.ArgumentParser(
     description='''\
     Connect to list of devices and run a set of commands on each.
     Format the Excel file as follows:
-            | DeviceName | OS_Type  | Implementation_Cmds |   Rollback_Cmds  |
-            | device1    | cisco    | commands to run     | rollback commands|
-            | device2    | juniper  | commands to run     | rollback commands|
-            |   etc...   | type     | commands to run     | rollback commands|
-        (OS_Type can be either "juniper", "cisco_ios", or "cisco_ios_telnet")
-        ''')
+        | Column A   | Column B |     Column C        |     Column D     |
+        +------------+----------+---------------------+------------------+
+    Row1| DeviceName | OS_Type  | Implementation_Cmds |   Rollback_Cmds  |
+    Row2| device1    | cisco    | commands to run     | rollback commands|
+    Row3| device2    | juniper  | commands to run     | rollback commands|
+    RowX|   etc...   | type     | commands to run     | rollback commands|
+    (OS_Type can be either "juniper", "cisco_ios", or "cisco_ios_telnet")
+    Optional: Include 'show' commands in Column E for on-screen
+    verification of your implmentation commands (will be logged as well).
+
+    Note: If the '-v' switch is given, you will be prompted to save changes,
+    otherwise, changes are saved silently.
+    ''')
 always_required = parser.add_argument_group('always required')
 always_required.add_argument("input_xlsx", nargs=1, help="Name of file containing \
                              devices and commands for each device",
                              metavar='<import_file>')
+parser.add_argument('-v', '--verify',
+                    help="Ask for verification before saving",
+                    action="store_true")
 parser.add_argument('-r', '--rollback', help="Run rollback commands",
                     action="store_true")
+
 args = parser.parse_args()
 
 # Configure logging
@@ -54,19 +65,88 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
+def main():
+    input_file = args.input_xlsx[0]
+    input_info = open_file(input_file)
+
+    username, password = get_creds()
+
+    netmiko_exceptions = (netmiko.ssh_exception.NetMikoTimeoutException,
+                          netmiko.ssh_exception.NetMikoAuthenticationException)
+
+    # Build dictionary of devices
+    for device_name in input_info.keys():
+        input_device = input_info[device_name]
+        device_dict = {'host': device_name,
+                       'device_type': input_device['device_type'],
+                       'username': username,
+                       'password': password,
+                       'secret': password
+                       }
+        print('Connecting to ' + device_dict['host'] + ' (' +
+              device_dict['device_type'] + ') ...')
+        try:  # Connect to device
+            connection = netmiko.ConnectHandler(**device_dict)
+            logger.info('Successfully connected to %s', device_dict['host'])
+            connection.enable()
+
+            # Send commands
+            if args.rollback:
+                print('Sending rollback commands...')
+                result = connection.send_config_set(
+                                    input_device['rollback_cmds'])
+            else:
+                print('Sending implementation commands...')
+                result = connection.send_config_set(
+                                    input_device['implementation_cmds'])
+            indented_lines = indentem(result)
+            logger.info('Actions: \n%s', indented_lines)
+
+            # Run 'show' commands, if present
+            if input_device['verification_cmds']:
+                verify_config(connection, input_device['verification_cmds'])
+
+            # Determine whether to save
+            if args.verify:
+                yes_or_no = ask_to_save()
+                if yes_or_no == 'y':
+                    print('Saving config changes...')
+                    save_now(connection, device_dict['device_type'])
+                    logger.info('Saved config changes on %s',
+                                device_dict['host'])
+                else:
+                    print('Changes NOT saved!')
+                    logger.info('Changes NOT saved, roll back or reboot')
+            else:
+                save_now(connection, device_dict['device_type'])
+                logger.info('Saved changes on %s' % device_dict['host'])
+
+            # Disconnect from device
+            print('Disconnecting...')
+            logger.info('Disconnecting from %s' % device_dict['host'])
+            connection.disconnect()
+
+        except netmiko_exceptions as e:
+            print('Failed to connect: %s' % e)
+            logger.error('Failed to connect %s', e)
+    print('Completed. See "output.log" for results.')
+
+
 def open_file(file):
     wb = openpyxl.load_workbook(file)
     ws = wb.active
-    devices = []
-    device_type = []
-    implementation_cmds = []
-    rollback_cmds = []
-    for row in range(2, ws.max_row):
-        devices.append(ws['A' + str(row)].value)
-        device_type.append(ws['B' + str(row)].value)
-        implementation_cmds.append(ws['C' + str(row)].value)
-        rollback_cmds.append(ws['D' + str(row)].value)
-    return devices, device_type, implementation_cmds, rollback_cmds
+    input_info = {}
+    for row in range(2, ws.max_row + 1):
+        device = ws['A' + str(row)].value
+        input_info = {device:
+                      {'host': device,
+                       'device_type': ws['B' + str(row)].value,
+                       'implementation_cmds': ws['C' + str(row)].value,
+                       'rollback_cmds': ws['D' + str(row)].value,
+                       'verification_cmds': ws['E' + str(row)].value
+                       }
+                      }
+    return input_info
 
 
 def get_creds():  # Prompt for credentials
@@ -76,50 +156,31 @@ def get_creds():  # Prompt for credentials
     return username, password
 
 
-def main():
-    input_file = args.input_xlsx[0]
-    devices, device_type, implementation_cmds, rollback_cmds \
-        = open_file(input_file)
-    username, password = get_creds()
+def verify_config(connection, commands):
+    proof = connection.send_command(commands)
+    print(proof)
+    logger.info('Verifcation commands results: \n%s', proof)
+    return
 
-    netmiko_exceptions = (netmiko.ssh_exception.NetMikoTimeoutException,
-                          netmiko.ssh_exception.NetMikoAuthenticationException)
 
-    counter = 0
-    for a_device in devices:
-        device_dict = {'host': a_device,
-                       'device_type': device_type[counter],
-                       'username': username,
-                       'password': password,
-                       'secret': password
-                       }
-        print('Connecting to ' + device_dict['host'] + ' (' +
-              device_dict['device_type'] + ') ...')
-        try:
-            connection = netmiko.ConnectHandler(**device_dict)
-            logger.info('Successfully connected to %s', device_dict['host'])
-            connection.enable()
-            print('Sending commands...')
-            if args.rollback:
-                result = connection.send_config_set(rollback_cmds[counter])
-            else:
-                result = connection.send_config_set(implementation_cmds
-                                                    [counter])
-            logger.info('Actions: \n%s', result)
-            print('Saving config changes...')
-            if device_dict['device_type'] in ['cisco_ios', 'cisco_ios_telnet']:
-                connection.send_command('write mem')
-            elif device_dict['device_type'] == 'juniper':
-                connection.send_config_set('commit')
-            logger.info('Saved config changes on %s', device_dict['host'])
-            print('Disconnecting')
-            connection.disconnect()
-            counter += 1
+def ask_to_save():
+    answer = raw_input('Save changes? (y/n) ')
+    if answer.lower() == 'y':
+        return answer
 
-        except netmiko_exceptions as e:
-            print('Failed to connect: %s' % e)
-            logger.error('Failed to connect %s', e)
-    print('Completed. See "output.log" for results.')
+
+def save_now(connection, device_type):
+    print('Saving config changes...')
+    if device_type in ['cisco_ios', 'cisco_ios_telnet']:
+        connection.send_command('write mem')
+    elif device_type == 'juniper':
+        connection.send_config_set('commit')
+
+
+def indentem(lines):
+    lines = '    ' + lines
+    lines = lines.replace('\n', '\n    ')
+    return lines
 
 
 main()
